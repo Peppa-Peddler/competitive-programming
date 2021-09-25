d12d74761fcaf48ad889af12c07bd3f9720fe11a
peppa-peddler <jorenmija@gmail.com>
2021-09-25 18:43:25 -0500

    adding factorial and inverses

---
+++ number-theory/preinv.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+#define LL long long
+#define N5 100005
+
+#define MOD 1000000007
+
+using namespace std;
+
+int INV[ N5 ];
+LL F[ N5 ];
+LL FI[ N5 ];
+
+void prefac(int Nmax, int mod){
+  INV[ 1 ] = 1;
+  for( LL i = 2; i < Nmax ; i++ )
+    INV[ i ] = mod - (LL)( mod / i ) * INV[ mod % i ] % mod;
+
+  F[ 0 ] = 1;
+  for( LL i = 1 ; i < Nmax ; i++ )
+    F[ i ] = F[ i - 1 ]*i % mod;
+
+  FI[ 0 ] = 1;
+  for( LL i = 1 ; i < Nmax ; i++ )
+    FI[ i ] = FI[ i - 1 ]*INV[ i ] % mod;
+}
+
+
+int main(){
+  prefac(N5, 31);
+  for(int i = 1; i <= 10; i++){
+    cout << F[ i ] << " " <<  INV[ i ] << endl;
+  }
+}
