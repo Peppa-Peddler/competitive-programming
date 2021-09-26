6ce32041fe5c21e55397ae5002b34cac57f53a0e
peppa-peddler <jorenmija@gmail.com>
2021-09-25 21:48:27 -0500

    CF 420c div 1 / combinatorics / graphs

---
+++ TLE1700/420c.cpp
@@ -0,0 +1,125 @@
+#include<bits/stdc++.h>
+
+#define LL long long
+
+#define PII pair<int, int>
+#define VI vector<int>
+#define PB push_back
+#define PQ priority_queue
+
+#define MP make_pair
+#define F first
+#define S second
+
+#define MOD 1000000007
+#define N6 1000006
+#define N5 100005
+#define N4 10004
+#define N3 1003
+
+#define PI 3.1415926535897932384626
+
+#define clr(v, a) memset(v, a, sizeof(v))
+#define all(x) x.begin(),x.end()
+#define R(i, n) for(int i=0; i<n; i++)
+#define len(x) ((int)(x).size())
+
+#define bits(x) __builtin_popcount(x)
+#define bitsLL(x) __builtin_popcountll(x)
+
+#define zerosl(x) __builtin_clz(x)
+#define zerosr(x) __builtin_ctz(x)
+
+/*
+⢸⣿⣿⣿⣿⠃⠄⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀⠄
+⢸⣿⣿⣿⡟⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷
+⢸⣿⣿⠟⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿
+⢸⣿⢫⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿
+⡿⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿
+⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿
+⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟
+⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣
+⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾
+⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿
+⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿
+⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿
+⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿
+⠄⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋⠄⠄⣾⡌⢠⣿⡿⠃
+⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉⠄⠄
+*/
+
+using namespace std;
+
+int n, p, k, t = 1;
+
+int N[ 3*N5 ];
+int s[ 3*N5 ]={};
+
+PII ps[ 3*N5 ];
+
+LL solve(){
+  cin >> n >> p;
+
+  int a, b;
+
+  R(i, n){
+    scanf("%d %d", &a, &b);
+    a--, b--;
+    ps[ i ].F = min(a, b);
+    ps[ i ].S = max(a, b);
+    N[ a ]++;
+    N[ b ]++;
+  }
+
+  R(i, n){
+    s[ N[ i ] ]++;
+  }
+
+  int prev = 0;
+  R(i, n){
+    prev += s[ n - i - 1 ];
+    s[ n - i - 1 ] = prev;
+  }
+
+  LL ans = 0;
+
+  R(i, n){
+    if( p <= N[ i ] ) {ans += n - 1; continue;}
+    ans += s[ p - N[ i ] ];
+    if( N[ i ] >= p - N[ i ] ) ans --;
+  }
+
+  ans /= 2;
+
+  sort( ps, ps + n );
+  int c = 1, i = 1, sum;
+
+  while( i < n ){
+
+    if( ps[ i ] == ps[ i - 1 ] ){
+      c++;
+    } else {
+      a = ps[ i - 1 ].F;
+      b = ps[ i - 1 ].S;
+      sum = N[ a ] + N[ b ];
+      if( sum >= p and sum - c < p ) ans--;
+      c = 1;
+    }
+    i++;
+  }
+
+  a = ps[ i - 1 ].F;
+  b = ps[ i - 1 ].S;
+  sum = N[ a ] + N[ b ];
+  if( sum >= p and sum - c < p ) ans--;
+
+  return ans;
+}
+
+int main(){
+
+    //cin >> t;
+    while(t--) cout << solve() << endl;
+    //while(t--) solve();
+
+}
