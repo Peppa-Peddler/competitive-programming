c9ec40e8461047cff241f8bb7624d456d2ec9825
peppa-peddler <jorenmija@gmail.com>
2021-09-28 16:00:33 -0500

    updating template

--- template.cpp
+++ template.cpp
@@ -1,68 +1,67 @@
 #include<bits/stdc++.h>
 
-#define LL long long
-
-#define PII pair<int, int>
-#define VI vector<int>
 #define PB push_back
+#define PF push_front
+#define QTOP front
+#define STOP top
+#define PQTOP top
+
 #define PQ priority_queue
 
 #define MP make_pair
 #define F first
 #define S second
 
-#define MOD 1000000007
+#define X first
+#define Y second.first
+#define Z second.second
+
+#define MOD7 1000000007
+#define MOD9 1000000009
+#define MOD 998244353
+
 #define N6 1000006
 #define N5 100005
 #define N4 10004
 #define N3 1003
-
 #define PI 3.1415926535897932384626
 
-#define N N5
-
-#define clr(v, a) memset(v, a, sizeof(v))
+#define fill(x, v) memset(x, v, sizeof(x))
 #define all(x) x.begin(),x.end()
-#define R(i, n) for(int i=0; i<n; i++)
+#define R(i, n) for(ll i = 0; i < ll(n) ; i++)
+
+#define FER(i,a,b) for(ll i=ll(a); i< ll(b); ++i)
+#define REF(i,a,b) for(ll i=ll(a); i>=ll(b); --i )
+
 #define len(x) ((int)(x).size())
+#define elif else if
 
-#define bits(x) __builtin_popcount(x)
-#define bitsLL(x) __builtin_popcountll(x)
 
-#define zerosl(x) __builtin_clz(x)
-#define zerosr(x) __builtin_ctz(x)
+#define sqr(x) 1LL * (x) * (x)
+#define INF (ll) 1e15
 
-/*
-⢸⣿⣿⣿⣿⠃⠄⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀⠄
-⢸⣿⣿⣿⡟⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷
-⢸⣿⣿⠟⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿
-⢸⣿⢫⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿
-⡿⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿
-⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿
-⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟
-⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣
-⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾
-⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿
-⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿
-⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿
-⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿
-⠄⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋⠄⠄⣾⡌⢠⣿⡿⠃
-⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉⠄⠄
-*/
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0)
 
 using namespace std;
 
-int n, m, k, t = 1;
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef pair<ll, ii > tri;
+typedef vector<ll> vi;
+typedef vector<ii> vii;
 
-LL solve(){
-    cin >> n;
+#define N N5
+
+ll n, m, t = 1, ans;
+
+ll solve(){
+	cin >> n;
     return 0;
 }
 
 int main(){
-
+	fastio;
     //cin >> t;
     while(t--) cout << solve() << endl;
     //while(t--) solve();
-
 }
